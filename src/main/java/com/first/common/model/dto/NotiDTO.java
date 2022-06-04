02486fd4659fb690c53db7cf7d9ddbeea0bac034
Yejin Carol Chang <81130006+Yejin-Carol@users.noreply.github.com>
2022-06-04 13:19:43 +0900

    2022-06-04 git branch

---
+++ src/main/java/com/first/common/model/dto/NotiDTO.java
@@ -0,0 +1,24 @@
+package com.first.common.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class NotiDTO {
+
+    private int noti_id;
+    private int user_id;
+    private int noti_cat_id;// 1. 일반, 2. 공지, 3. 커뮤니티, 4. 
+    private String noti_title;
+    private String noti_content;
+    private String noti_reg_date;
+    private String noti_mod_date;
+    private int noti_count; //조회
+    private int noti_bookmark; //북마크
+
+}
